a3617b8cd4a47c1110a618e0e298604142a3263c
Chris Ball <chris@echobind.com>
2016-08-21 15:28:35 -0400

    start to processing blog md files

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 /* jshint node: true */
 'use strict';
 
+const Funnel = require('broccoli-funnel');
+const MergeTrees = require('broccoli-merge-trees');
+const BlogMarkdownParser = require('./lib/blog-markdown-parser');
+
 module.exports = {
-  name: 'ember-static-blog'
+  name: 'ember-static-blog',
+
+  preprocessTree(type, tree) {
+    var blogFiles = new Funnel(tree, {
+      srcDir: 'dummy/blog'
+    });
+
+    var blogTree = new BlogMarkdownParser(blogFiles);
+
+    return new MergeTrees([tree, blogTree]);
+  },
 };
