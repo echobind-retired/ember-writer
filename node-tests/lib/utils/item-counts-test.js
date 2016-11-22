13adae71b601558e479770c2a634569e90dd712c
Taras Mankovski <tarasm@gmail.com>
2016-11-22 16:56:26 -0500

    item-counts no longer counts empty strings

--- node-tests/lib/utils/item-counts-test.js
+++ node-tests/lib/utils/item-counts-test.js
@@ -8,15 +8,29 @@ describe('itemCounts', function() {
   describe('with array', function() {
     let result;
 
-    beforeEach(function() {
-      let array = ['one', 'two', 'three', 'two', 'two', 'one'];
-      result = itemCounts(array);
+    describe('of many values', function() {
+      beforeEach(function() {
+        let array = ['one', 'two', 'three', 'two', 'two', 'one'];
+        result = itemCounts(array);
+      });
+
+      it('returns the counts in the result object', function() {
+        expect(result.one).to.equal(2);
+        expect(result.two).to.equal(3);
+        expect(result.three).to.equal(1);
+      });
     });
 
-    it('returns the counts in the result object', function() {
-      expect(result.one).to.equal(2);
-      expect(result.two).to.equal(3);
-      expect(result.three).to.equal(1);
+    describe('of empty values', function() {
+      beforeEach(function() {
+        let array = [''];
+        result = itemCounts(array);
+      });
+
+      it('ignores empty strings', function() {
+        expect(result).to.deep.equal({});
+      });
     });
+
   });
 });
